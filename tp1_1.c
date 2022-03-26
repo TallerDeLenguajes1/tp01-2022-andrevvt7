e85a4c407d24105a8543f35eff5496d8dca2afae
andrevvt7 <andrevvt7@gmail.com>
2022-03-26 18:06:52 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,12 @@
 
 int main(){
 
-    printf("Hola mundo");
+    int num = 7, *pnum;
+    pnum = &num;
+
+    printf("Hola mundo\n\n");
+
+    printf("Contenido del puntero: %d\nDirecci%cn de memoria almacenada por el puntero: %p\nDirecci%cn de memoria de la variable: %p\nDirecci%cn de memoria del puntero: %p\nTama%co de memoria utilizado por la variable: %d\n\n", *pnum, 162, pnum, 162, &num, 162, &pnum, 164, sizeof(num));
 
     return 0;
 }
